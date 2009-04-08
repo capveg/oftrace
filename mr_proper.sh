390bb82746dea920e59b2526e7c66e9d40f9524e
Rob Sherwood <rob.sherwood@stanford.edu>
2009-04-08 12:11:12 -0700

    added more autoconf stuff to cleanup list

--- mr_proper.sh
+++ mr_proper.sh
@@ -4,7 +4,7 @@ if [ -f Makefile ] ; then
 	make maintainer-clean
 fi
 rm -rf aclocal.m4 configure depcomp install-sh missing Makefile.in autom4te.cache config.status Makefile config.log
-rm -rf gmon.out
+rm -rf gmon.out m4
 # stupid emacs users
 rm -f *~
 # swit wrapper
