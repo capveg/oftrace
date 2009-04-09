f66b19e3304260ecf6feaf06ad07cfb7e5ad5b4e
Yiannis Yiakoumis <yiannisy@stanford.edu>
2009-04-08 20:08:51 -0700

    implemented pyofdump.py

--- pyofdump.py
+++ pyofdump.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
-import sys,socket
+import sys
+import struct
+from socket import *
 from optparse import OptionParser
-
+from oftrace import oftrace
 
 def main():
     usage = "usage: %prog [options] arg"
@@ -22,6 +24,51 @@ def main():
     print ("Reading %s from controller %s , port %d") % \
           (options.filename,options.controller,options.port)
 
+    do_analyze(options.filename,options.controller,options.port)
 
+def do_analyze(filename,controller,port):
+    msg_count = 0
+    start_time = {}
+    # a bit ugly...
+    ip = inet_pton(AF_INET,controller)
+    ip = struct.unpack("I",ip)[0]
+
+    oft = oftrace.oftrace_open(filename)
+    m = oftrace.oftrace_next_msg(oft,ip,port)
+
+    # save this for timestamp reference
+    if (m != None):
+        start_time["sec"] = m.phdr.ts_sec
+        start_time["usec"] = m.phdr.ts_usec
+    # dump all packets
+    while ( m != None):
+        msg_count = msg_count+1
+        dump_msg(msg_count,m,start_time)
+        m = oftrace.oftrace_next_msg(oft,ip,port)
+
+def dump_msg(msg_count,m,start_time):
+    curr_time = {}
+    curr_time["sec"] = m.phdr.ts_sec - start_time["sec"]
+    if(m.phdr.ts_usec < start_time["usec"]):
+        curr_time["usec"] = m.phdr.ts_usec + 1000000 - start_time["usec"]
+        curr_time["sec"] = curr_time["sec"] - 1
+    else:
+        curr_time["usec"] = m.phdr.ts_usec - start_time["usec"]
+
+    src_ip = inet_ntop(AF_INET,struct.pack("I",m.ip.saddr))
+    dst_ip = inet_ntop(AF_INET,struct.pack("I",m.ip.daddr))
+    
+    
+    print("%d. %6d.%6d: FROM %s:%u\tTO %s:%u\t OFP_TYPE %d LEN %d") % \
+               (msg_count,
+                curr_time["sec"],
+                curr_time["usec"],
+                src_ip,
+                ntohs(m.tcp.source),
+                dst_ip,
+                ntohs(m.tcp.dest),
+                m.ofph.type,
+                ntohs(m.ofph.length))
+                      
 if __name__ == "__main__":
     main()
